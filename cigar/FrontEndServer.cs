977925be5f740cd7a356be93f3062a8496b310f7
20chan <2@0chan.dev>
2019-12-27 17:49:47 +0900

    baseurl+

--- cigar/FrontEndServer.cs
+++ cigar/FrontEndServer.cs
@@ -11,11 +11,12 @@ namespace cigar {
             Index = index;
         }
 
+        [Get("/cigar")]
         [Get("/cigar/{f0}")]
         [Get("/cigar/{f0}/{f1}")]
         [Get("/cigar/{f0}/{f1}/{f2}")]
         public Response RouteFile(Request req) {
-            var path = req.Uri.AbsolutePath.Substring(1);
+            var path = req.Uri.AbsolutePath.Substring(6).TrimStart('/');
             if (string.IsNullOrEmpty(path)) {
                 path = Index;
             }
